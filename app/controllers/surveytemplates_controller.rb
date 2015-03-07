765e4e97f3a0ce392bee010165792cc555c84a5a
Ben_Ubun <mabinogiben@berkeley.edu>
2015-03-06 21:28:49 -0800

    working view for surveytemplate

--- app/controllers/surveytemplates_controller.rb
+++ app/controllers/surveytemplates_controller.rb
@@ -1,5 +1,12 @@
 class SurveytemplatesController < ApplicationController
 	def index
 		#@stemplates = SurveyTemplate.survey_fields
+		@stemplates = [1,2,3,4]
+	end
+
+	def show
+		id = params[:id]
+		#@stemplates = SurveyTemplate.find(id).survey_fields
+		@stemplates = [1,2,3,4]
 	end
 end
