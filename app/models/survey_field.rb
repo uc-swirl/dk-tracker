5fa76e91a1bea9cd7d5eaece8d54bfebf5340b90
Alex Lin <alexlily@berkeley.edu>
2015-03-07 03:18:31 -0400

    ran a migration for submissions table and added some has_many/belongs_to to fields, and made a field_response model

--- app/models/survey_field.rb
+++ app/models/survey_field.rb
@@ -1,4 +1,5 @@
 class SurveyField < ActiveRecord::Base
   attr_accessible :question_title
   belongs_to :survey_template
+  has_many :field_responses
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,15 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20150307013701) do
+ActiveRecord::Schema.define(:version => 20150307061309) do
+
+  create_table "field_responses", :force => true do |t|
+    t.integer  "survey_field_id"
+    t.integer  "submission_id"
+    t.string   "response"
+    t.datetime "created_at",      :null => false
+    t.datetime "updated_at",      :null => false
+  end
 
   create_table "movies", :force => true do |t|
     t.string   "title"
@@ -19,6 +27,11 @@ ActiveRecord::Schema.define(:version => 20150307013701) do
     t.string   "rating"
     t.text     "description"
     t.string   "director"
+  end
+
+  create_table "submissions", :force => true do |t|
+    t.datetime "created_at", :null => false
+    t.datetime "updated_at", :null => false
   end
 
   create_table "survey_fields", :force => true do |t|
