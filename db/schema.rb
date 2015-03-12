6ebf8f4a135c5add1094056349e8247558e9714c
Alex Lin <=>
2015-03-12 20:07:06 -0300

    made user class tests pass with devise, which involved making the passwords > 8 characters each

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20150312042511) do
+ActiveRecord::Schema.define(:version => 20150312223619) do
 
   create_table "survey_fields", :force => true do |t|
     t.string   "type"
