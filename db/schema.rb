fa8a2e4e0305cf31f67e2d4a30ba46fccf0d4559
Dorthy Luu <dluu@berkeley.edu>
2015-03-13 12:35:19 -0300

    saved changes to schema.rb to pull from master

--- db/schema.rb
+++ db/schema.rb
@@ -11,15 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20150307013701) do
-
-  create_table "movies", :force => true do |t|
-    t.string   "title"
-    t.datetime "release_date"
-    t.string   "rating"
-    t.text     "description"
-    t.string   "director"
-  end
+ActiveRecord::Schema.define(:version => 20150307031015) do
 
   create_table "survey_fields", :force => true do |t|
     t.string   "type"
@@ -35,6 +27,16 @@ ActiveRecord::Schema.define(:version => 20150307013701) do
   create_table "survey_templates", :force => true do |t|
     t.datetime "created_at", :null => false
     t.datetime "updated_at", :null => false
+  end
+
+  create_table "users", :force => true do |t|
+    t.string   "name"
+    t.string   "phone_number"
+    t.string   "email"
+    t.string   "password"
+    t.boolean  "active"
+    t.datetime "created_at",   :null => false
+    t.datetime "updated_at",   :null => false
   end
 
 end
