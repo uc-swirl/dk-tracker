43f3c76ef9e0a762c3f6fa266094d68def87b599
Alex Lin <=>
2015-03-13 04:39:21 -0300

    deleted some migration files and made submissions_controller spec

--- db/migrate/20150313000316_create_field_responses.rb
+++
@@ -1,9 +0,0 @@
-class CreateFieldResponses < ActiveRecord::Migration
-  def change
-    create_table :field_responses do |t|
-      t.references :survey_field
-      t.references :submissions
-      t.timestamps
-    end
-  end
-end
--- spec/controllers/submissions_controller_spec.rb
+++ spec/controllers/submissions_controller_spec.rb
@@ -1,5 +1,4 @@
 require 'spec_helper'
 
 describe SubmissionsController do
-
 end
