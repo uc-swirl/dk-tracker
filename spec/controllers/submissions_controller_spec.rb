4f76d1f6af36959c8274e16ae27bae81d04b0793
Marco Montagna <marcojoemontagna@gmail.com>
2015-03-13 18:30:10 -0700

    Fixed some controller tests

--- spec/controllers/submissions_controller_spec.rb
+++ spec/controllers/submissions_controller_spec.rb
@@ -2,24 +2,30 @@ require 'spec_helper'
 
 describe SubmissionsController do
   it 'adds a submission to the submissions table' do
-    SurveyField.stub(:find_the_id).and_return(1)
-    FieldResponse.stub(:survey_field).and_return(TextQuestionField)
+    st = SurveyTemplate.create
+    f1 = st.text_question_fields.build(:question_title => "Poodles?") 
+    st.save!   
     expect{
-      post :create, {:submission => {1 => "another response", 2 => "another response"}}
+      post :create, {:submission => {f1.id => "another response"}}
     }.to change{Submission.all.length}.by(1)
   end
   
   it 'adds responses to the field_responses table'do
-    SurveyField.stub(:find_the_id).and_return(1)
-    
+    st = SurveyTemplate.create
+    f1 = st.text_question_fields.build(:question_title => "PoOdles?") 
+    f2 = st.text_question_fields.build(:question_title => "TOodles!") 
+    st.save! 
+
     expect{
-      post :create, {:submission => {1 => "another response", 2 => "another response"}}
+      post :create, {:submission => { f1.id => "another response", f2.id => "another response"}}
     }.to change{FieldResponse.all.length}.by(2)
   end
   
   it 'goes back to the survey templates page' do
-    SurveyField.stub(:find_the_id).and_return(1)
-    post :create, {:submission => {1 => "another response", 2 => "another response"}}
+    st = SurveyTemplate.create
+    f1 = st.text_question_fields.build(:question_title => "Poodles?") 
+    st.save!   
+    post :create, {:submission => {f1.id => "another response"}}
     response.should redirect_to survey_templates_path
   end
 end
